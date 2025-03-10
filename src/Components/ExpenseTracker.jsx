40e0ec675efb3e61baaf65da4f47628939d61b58
Sukrut11 <87969705+Sukrut11@users.noreply.github.com>
2025-03-10 19:10:28 +0000

    Changed expense categories

--- src/Components/ExpenseTracker.jsx
+++ src/Components/ExpenseTracker.jsx
@@ -6,12 +6,12 @@ export default function ExpenseTracker(props) {
   const [expenseDetails, createExpenseDetails] = useState({
     expenseName: "",
     expenseAmount: "",
-    expenseCategory: "Personal Expense",
+    expenseCategory: "Food Expense",
     expenseDate: "",
   });
 
   const [expenseListArrayContents, updateExpenseListArrayContents] = useState(
-    JSON.parse(localStorage.getItem("expenses")) || []
+    JSON.parse(localStorage.getItem("expenses")) || [],
   );
 
   const [fetchedDate, setFetchedDate] = useState({
@@ -47,21 +47,28 @@ export default function ExpenseTracker(props) {
     createExpenseDetails({
       expenseName: "",
       expenseAmount: "",
-      expenseCategory: "Personal Expense",
+      expenseCategory: "Food Expense",
       expenseDate: "",
     });
 
-    showAlert("success", "Your expense has been added.")
+    showAlert("success", "Your expense has been added.");
 
     // Show warning toast to user if he is creating expense while having an active filter.
     // Check if any date filter is currently active
     if (fetchedDate.activeDate && fetchedDate.activeDateType) {
-      const currentFilteredExpenses = filteredExpenses(fetchedDate.activeDate, fetchedDate.activeDateType)
+      const currentFilteredExpenses = filteredExpenses(
+        fetchedDate.activeDate,
+        fetchedDate.activeDateType,
+      );
       const isExpenseApplicableInFilter = currentFilteredExpenses.some(
-        (expense) => expense.expenseDate === formattedDetails.expenseDate) // implicit return
+        (expense) => expense.expenseDate === formattedDetails.expenseDate,
+      ); // implicit return
 
       if (!isExpenseApplicableInFilter) {
-        showAlert("warning", "New expense added but it's outside the current filter. Please clear filter to view all expenses.")
+        showAlert(
+          "warning",
+          "New expense added but it's outside the current filter. Please clear filter to view all expenses.",
+        );
       }
       // console.log(isExpenseApplicableInFilter)
     }
@@ -77,23 +84,22 @@ export default function ExpenseTracker(props) {
 
   // Function to remove latest expense from the array
   const removeLatestExpense = () => {
-    if(expenseListArrayContents.length > 0){
+    if (expenseListArrayContents.length > 0) {
       updateExpenseListArrayContents((prevContentsOfArray) =>
-        prevContentsOfArray.slice(0, -1)
+        prevContentsOfArray.slice(0, -1),
       );
-      showAlert("success", "Your latest expense has been removed.")
-    }
-    else{
-      return
+      showAlert("success", "Your latest expense has been removed.");
+    } else {
+      return;
     }
   };
 
   // Remove particular expense from list
   const removeParticularElementFromList = (indexOfExpense) => {
     updateExpenseListArrayContents((prevContentsOfArray) =>
-      prevContentsOfArray.filter((_, index) => index !== indexOfExpense)
+      prevContentsOfArray.filter((_, index) => index !== indexOfExpense),
     );
-    showAlert("success", "Selected expense has been removed.")
+    showAlert("success", "Selected expense has been removed.");
   };
 
   // Format date into desired format eg - 07 Jan 25
@@ -113,18 +119,18 @@ export default function ExpenseTracker(props) {
 
   // Month Name object
   const monthNames = {
-    "Jan": 0,
-    "Feb": 1,
-    "Mar": 2,
-    "Apr": 3,
-    "May": 4,
-    "Jun": 5,
-    "Jul": 6,
-    "Aug": 7,
-    "Sep": 8,
-    "Oct": 9,
-    "Nov": 10,
-    "Dec": 11,
+    Jan: 0,
+    Feb: 1,
+    Mar: 2,
+    Apr: 3,
+    May: 4,
+    Jun: 5,
+    Jul: 6,
+    Aug: 7,
+    Sep: 8,
+    Oct: 9,
+    Nov: 10,
+    Dec: 11,
   };
 
   // Filter logic for Expense Details
@@ -156,12 +162,12 @@ export default function ExpenseTracker(props) {
         const firstDate = new Date(
           `20${firstYear}`,
           monthNames[firstMonth],
-          firstDay
+          firstDay,
         );
         const lastDate = new Date(
           `20${lastYear}`,
           monthNames[lastMonth],
-          lastDay
+          lastDay,
         );
 
         // Parse the expense date into a Date object
@@ -169,7 +175,7 @@ export default function ExpenseTracker(props) {
         const expenseDateObj = new Date(
           `20${expenseYear}`,
           monthNames[expenseMonth],
-          expenseDay
+          expenseDay,
         );
 
         // Log the parsed dates for debugging
@@ -188,7 +194,7 @@ export default function ExpenseTracker(props) {
         // Parse fetchedDate as month index and get corresponding month name
         const selectedMonthIndex = parseInt(fetchedDate, 10);
         const selectedMonth = Object.keys(monthNames).find(
-          (month) => monthNames[month] === selectedMonthIndex
+          (month) => monthNames[month] === selectedMonthIndex,
         );
 
         // Current year (last two digits)
@@ -214,52 +220,65 @@ export default function ExpenseTracker(props) {
 
   const clearFilterValues = () => {
     if (fetchedDate.activeDate === "" && fetchedDate.activeDateType === "") {
-      return
-    }
-    else{
+      return;
+    } else {
       setFetchedDate({ activeDateType: "", activeDate: "" });
-      showAlert("success", "Date filter has been cleared.")
+      showAlert("success", "Date filter has been cleared.");
     }
   };
 
   // To display total expense Amount to user
-  const [totalExpense, setTotalExpense] = useState(0)
+  const [totalExpense, setTotalExpense] = useState(0);
   useEffect(() => {
-    const expensesToConsider = (fetchedDate.activeDate && fetchedDate.activeDateType) ? filteredExpenses(fetchedDate.activeDate, fetchedDate.activeDateType) : expenseListArrayContents
-    const totalExpenseAmount = expensesToConsider.map((expense) => Number(expense.expenseAmount))
-      .reduce((sum, amount) => sum + amount, 0)
-    setTotalExpense(totalExpenseAmount)
-  }, [expenseListArrayContents, fetchedDate])
+    const expensesToConsider =
+      fetchedDate.activeDate && fetchedDate.activeDateType
+        ? filteredExpenses(fetchedDate.activeDate, fetchedDate.activeDateType)
+        : expenseListArrayContents;
+    const totalExpenseAmount = expensesToConsider
+      .map((expense) => Number(expense.expenseAmount))
+      .reduce((sum, amount) => sum + amount, 0);
+    setTotalExpense(totalExpenseAmount);
+  }, [expenseListArrayContents, fetchedDate]);
 
   // to show alert when user selects any filter
   useEffect(() => {
     if (fetchedDate.activeDate && fetchedDate.activeDateType === "day") {
-      showAlert("success", `Filter has been set for ${formatDate(fetchedDate.activeDate)}`);
-    }
-    else if (fetchedDate.activeDate && fetchedDate.activeDateType === "week") {
+      showAlert(
+        "success",
+        `Filter has been set for ${formatDate(fetchedDate.activeDate)}`,
+      );
+    } else if (
+      fetchedDate.activeDate &&
+      fetchedDate.activeDateType === "week"
+    ) {
       showAlert("success", `Filter has been set for ${fetchedDate.activeDate}`);
-    }
-
-    else if (fetchedDate.activeDate && fetchedDate.activeDateType === "month") {
+    } else if (
+      fetchedDate.activeDate &&
+      fetchedDate.activeDateType === "month"
+    ) {
       const selectedMonth = Object.keys(monthNames)[fetchedDate.activeDate];
       const currentYear = new Date().getFullYear().toString();
-      showAlert("success", `Filter has been set for ${selectedMonth} ${currentYear}`);
-    }
-
-    else if (fetchedDate.activeDate && fetchedDate.activeDateType === "year") {
+      showAlert(
+        "success",
+        `Filter has been set for ${selectedMonth} ${currentYear}`,
+      );
+    } else if (
+      fetchedDate.activeDate &&
+      fetchedDate.activeDateType === "year"
+    ) {
       showAlert("success", `Filter has been set for ${fetchedDate.activeDate}`);
     }
-  }, [fetchedDate])
+  }, [fetchedDate]);
 
   // alert toast code
-  const [alert, setAlert] = useState(null)
+  const [alert, setAlert] = useState(null);
 
   const showAlert = (type, message) => {
     setAlert({
       type: type,
-      message: message
-    })
-  }
+      message: message,
+    });
+  };
 
   // Load data from localStorage on mount
   useEffect(() => {
@@ -273,13 +292,13 @@ export default function ExpenseTracker(props) {
   }, [expenseListArrayContents]);
 
   const alertClosureStatus = () => {
-    setAlert(null)
-  }
+    setAlert(null);
+  };
 
   return (
     <>
       <div className="container my-3">
-        <Alert alert={alert} alertClosureStatus = {alertClosureStatus}/>
+        <Alert alert={alert} alertClosureStatus={alertClosureStatus} />
         <h3 className="heading my-3" align="center">
           {props.expenseTrackerHeading}
         </h3>
@@ -315,15 +334,16 @@ export default function ExpenseTracker(props) {
               value={expenseDetails.expenseCategory}
               onChange={handleExpenseInputChange}
             >
-              <option>Personal Expense</option>
-              <option>Daily Expense</option>
-              <option>Utilities Expense</option>
+              <option>Food Expense</option>
               <option>Housing Expense</option>
-              <option>Tax Expense</option>
-              <option>Investment Expense</option>
-              <option>Non Recurring Expense</option>
+              <option>Shopping Expense</option>
+              <option>Travel Expense</option>
               <option>Health Care Expense</option>
-              <option>Miscellaneous expense</option>
+              <option>Entertainment Expense</option>
+              <option>Investment Expense</option>
+              <option>Education Expense</option>
+              <option>Donation Expense</option>
+              <option>One-Time Expense</option>
             </select>
           </div>
           <div className="form-group">
@@ -372,10 +392,17 @@ export default function ExpenseTracker(props) {
             <h3 className="my-2">No expenses created.</h3>
           ) : fetchedDate.activeDateType && fetchedDate.activeDate ? (
             // Case 2: Filter is applied
-            filteredExpenses(fetchedDate.activeDate, fetchedDate.activeDateType).length > 0 ? (
+            filteredExpenses(fetchedDate.activeDate, fetchedDate.activeDateType)
+              .length > 0 ? (
               // Case 2a: Filter applied and matching results found
-              <ol className="expense-list" style={{ textAlign: "left", paddingLeft: "0" }}>
-                {filteredExpenses(fetchedDate.activeDate, fetchedDate.activeDateType).map((expense, index) => (
+              <ol
+                className="expense-list"
+                style={{ textAlign: "left", paddingLeft: "0" }}
+              >
+                {filteredExpenses(
+                  fetchedDate.activeDate,
+                  fetchedDate.activeDateType,
+                ).map((expense, index) => (
                   <li className="expenseItem my-4 ml-3" key={index}>
                     {/* Expense Name & Date */}
                     <div className="container-fluid d-flex justify-content-between align-items-center mt-2 text-start">
@@ -385,15 +412,23 @@ export default function ExpenseTracker(props) {
 
                     {/* Expense Amount */}
                     <div className="container-fluid mt-2 text-start">
-                      <span><strong>Rs. </strong>{expense.expenseAmount}</span>
+                      <span>
+                        <strong>Rs. </strong>
+                        {expense.expenseAmount}
+                      </span>
                     </div>
 
                     {/* Expense Category & Remove Button */}
                     <div className="container-fluid d-flex justify-content-between align-items-center gap-2 mt-2">
-                      <span className="badge bg-info text-white px-2 py-2">{expense.expenseCategory}</span>
+                      <span className="badge bg-info text-white px-2 py-2">
+                        {expense.expenseCategory}
+                      </span>
 
-                      <button type="button" onClick={() => removeParticularElementFromList(index)}
-                        className="btn btn-danger px-2 py-1">
+                      <button
+                        type="button"
+                        onClick={() => removeParticularElementFromList(index)}
+                        className="btn btn-danger px-2 py-1"
+                      >
                         Remove Expense
                       </button>
                     </div>
@@ -404,19 +439,24 @@ export default function ExpenseTracker(props) {
               // Case 2b: Filter applied but no matching results
               <h3 className="my-2">
                 No expenses found for{" "}
-                {fetchedDate.activeDateType === "year"
-                  ? ` year ${fetchedDate.activeDate}` // Show year directly
-                  : fetchedDate.activeDateType === "week"
-                    ? fetchedDate.activeDate // Show week range directly
-                    : fetchedDate.activeDateType === "month"
-                      ? `${Object.keys(monthNames)[fetchedDate.activeDate]} ${new Date().getFullYear()}` // Show month and current year
-                      : formatDate(fetchedDate.activeDate) // Default case for day
-                }.
+                {
+                  fetchedDate.activeDateType === "year"
+                    ? ` year ${fetchedDate.activeDate}` // Show year directly
+                    : fetchedDate.activeDateType === "week"
+                      ? fetchedDate.activeDate // Show week range directly
+                      : fetchedDate.activeDateType === "month"
+                        ? `${Object.keys(monthNames)[fetchedDate.activeDate]} ${new Date().getFullYear()}` // Show month and current year
+                        : formatDate(fetchedDate.activeDate) // Default case for day
+                }
+                .
               </h3>
             )
           ) : (
             // Case 3: No filter applied, show all expenses
-            <ol className="expense-list" style={{ textAlign: "left", paddingLeft: "0" }}>
+            <ol
+              className="expense-list"
+              style={{ textAlign: "left", paddingLeft: "0" }}
+            >
               {expenseListArrayContents.map((expense, index) => (
                 <li className="expenseItem my-4 ml-3" key={index}>
                   {/* Expense Name & Date */}
@@ -427,15 +467,23 @@ export default function ExpenseTracker(props) {
 
                   {/* Expense Amount */}
                   <div className="container-fluid mt-2 text-start">
-                    <span><strong>Rs. </strong>{expense.expenseAmount}</span>
+                    <span>
+                      <strong>Rs. </strong>
+                      {expense.expenseAmount}
+                    </span>
                   </div>
 
                   {/* Expense Category & Remove Button - Mobile & Desktop Aligned */}
                   <div className="container-fluid d-flex justify-content-between align-items-center gap-2 mt-2">
-                    <span className="badge bg-info text-white px-2 py-2">{expense.expenseCategory}</span>
+                    <span className="badge bg-info text-white px-2 py-2">
+                      {expense.expenseCategory}
+                    </span>
 
-                    <button type="button" onClick={() => removeParticularElementFromList(index)}
-                      className="btn btn-danger px-2 py-1">
+                    <button
+                      type="button"
+                      onClick={() => removeParticularElementFromList(index)}
+                      className="btn btn-danger px-2 py-1"
+                    >
                       Remove Expense
                     </button>
                   </div>
